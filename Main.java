f3a68123ae56409ed270a01f3133fb9f75fa6784
unknown <kewinpriv@gmail.com>
2024-06-01 12:06:19 +0200

    Zadanie 2

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -12,49 +14,45 @@ public class Main extends Application {
 
     private TextField directoryPathField;
     private TextField searchField;
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private TextArea resultArea;
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("File Browser and Search");
-        initializeFields();
+        primaryStage.setTitle("Przeglądarka plików i wyszukiwanie");
 
-        Button browseButton = createBrowseButton();
-        Button searchButton = new Button("Search");
-
-        HBox hBox = new HBox(10);
-        hBox.getChildren().addAll(directoryPathField, browseButton);
-
-        VBox vBox = new VBox(10);
-        vBox.getChildren().addAll(hBox, searchField, searchButton);
-
-        Scene scene = new Scene(vBox, 600, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private void initializeFields() {
         directoryPathField = new TextField();
-        directoryPathField.setPromptText("Enter directory path");
+        directoryPathField.setPromptText("Wpisz ścieżkę do katalogu");
 
         searchField = new TextField();
-        searchField.setPromptText("Enter search phrase");
-    }
+        searchField.setPromptText("Wpisz frazę do wyszukania");
 
-    private Button createBrowseButton() {
-        Button browseButton = new Button("Browse");
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
+
+        Button browseButton = new Button("Przeglądaj");
         browseButton.setOnAction(e -> browseDirectory());
-        return browseButton;
+
+        Button searchButton = new Button("Szukaj");
+
+        HBox hBox = new HBox(10, directoryPathField, browseButton);
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
+
+        Scene scene = new Scene(vBox, 600, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     private void browseDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory = directoryChooser.showDialog(null);
+
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
